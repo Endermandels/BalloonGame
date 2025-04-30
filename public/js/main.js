67ebfb8401788e7e023620f23e073dfafca0757c
Endermandels <elijahdelavar@gmail.com>
2025-04-30 10:20:40 -0700

    Add explosion image

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,15 @@
 const balloon = document.querySelector(".balloon-svg");
+const explosion = document.querySelector(".explosion-img");
 
 let inflateInterval = null;
 let shrinkInterval = setInterval(() => {
-    if (balloon && balloon.style && scale > minScale) {
+    if (scale > minScale) {
         scale -= shrinkRate;
         balloon.style.transform = `scale(${scale})`;
     }
 }, 50); // update every 50ms
 
+let popped = false;
 let scale = 1;
 const baseSize = 100;
 const shrinkRate = 0.005;
@@ -21,25 +23,32 @@ balloon.addEventListener('mouseleave', onBalloonMouseUp); // mouse left the ball
 balloon.addEventListener('mouseup', onBalloonMouseUp);
 
 function onBalloonMouseDown() {
-    if (inflateInterval) return;
+    if (inflateInterval || popped) return;
 
     clearInterval(shrinkInterval);
     shrinkInterval = null;
     inflateInterval = setInterval(() => {
-        if (balloon && balloon.style && scale < maxScale) {
+        if (scale < maxScale) {
             scale += inflateRate;
             balloon.style.transform = `scale(${scale})`;
+        } else {
+            // Pop
+            popped = true;
+            balloon.classList.toggle("invisible");
+            explosion.hidden = false;
+            clearInterval(inflateInterval);
+            inflateInterval = null;
         }
     }, 10); // update every 50ms
 }
 
 function onBalloonMouseUp() {
-    if (shrinkInterval) return;
+    if (shrinkInterval || popped) return;
 
     clearInterval(inflateInterval);
     inflateInterval = null;
     shrinkInterval = setInterval(() => {
-        if (balloon && balloon.style && scale > minScale) {
+        if (scale > minScale) {
             scale -= shrinkRate;
             balloon.style.transform = `scale(${scale})`;
         }
